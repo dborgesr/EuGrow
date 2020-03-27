cc557d4a9422fc66180b535849aac78968d8a91e
Diego Borges <dborgesrmit@gmail.com>
2020-03-26 23:14:34 -0400

    adding minitd

--- Nanotid/applotid.py
+++ Nanotid/applotid.py
@@ -21,12 +21,16 @@ from datetime import datetime as dt
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import timezone
-import rpi_rf
-from rpi_rf import RFDevice
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-#GPIO.setwarnings(False)
-#GPIO.setmode(GPIO.BCM)
+
+#RPI-RF for 433 control
+rpi_rf_433 = False
+if (rpi_rf_433):
+        import rpi_rf
+        from rpi_rf import RFDevice
+        import RPi.GPIO as GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
 
 # Set up Dash app and database
 server = Flask('applotid')
@@ -51,14 +55,15 @@ cur_jobs = list()
 
 #create tables, may need to make sure to not create if they exist already
 #db.create_all()
+
 #start Dash app
-app = dash.Dash('applotid-front', server=server,csrf_protect=False)
-
-#Add css style to dash app
-app.css.append_css({
-        "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
-})
-
+app = dash.Dash(
+    'applotid-front',
+    external_stylesheets=[
+        'https://codepen.io/chriddyp/pen/bWLwgP.css'
+    ],
+    server=server
+)
 ###########################
 # Data Manipulation / Model
 ###########################
@@ -395,9 +400,9 @@ dat_opts.append(dict(label=str('*'), value=str('*')))
 
 rf_code_button = ['RF1','RF2','RF3','RF4','RF5','NA','NA','NA','NA','NA']
 rf_code_on = [5264691,5264835,5265155,5266691,5272835,267571,267715,268035,269571,275715]
-rf_code_on_time = [184,184,184,184,184,185,187,186,186,186]
+rf_code_on_time = [184,184,184,184,184,190,190,190,190,190]
 rf_code_off = [5264700,5264844,5265164,5266700,5272844,267580,267724,268044,269580,275724]
-rf_code_off_time = [184,184,184,184,184,185,187,186,186,186]
+rf_code_off_time = [188,188,188,188,188,190,190,190,190,190]
 columns_rf = { "RF Button":rf_code_button, "RF code ON":rf_code_on, "RF code ON timing":rf_code_on_time, "RF code OFF":rf_code_off, "RF code OFF timing":rf_code_off_time }
 rf_df = pd.DataFrame(data=columns_rf)
 
@@ -816,55 +821,90 @@ def load_humidity_graph(date_enviro_submit, start_date, end_date):
         [Input(component_id='rf_on_button1', component_property='n_clicks')])
 def rf1_on(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5264691, tx_pulselength=184)
-                rfdevice.cleanup()
-                #subprocess.check_output("/var/www/rfoutlet/codesend 5264691 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5264691, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5264691 -l 184 -p 0", shell=True) 
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5264691 0 188", shell=True)
+                                time.sleep(5)
         return
 @app.callback(
         Output(component_id='placeholder2', component_property='children'),
         [Input(component_id='rf_on_button2', component_property='n_clicks')])
 def rf2_on(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5264835, tx_pulselength=184)
-                rfdevice.cleanup()
-                #subprocess.check_output("/var/www/rfoutlet/codesend 5264835 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5264835, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5264835 -l 184 -p 0", shell=True)
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5264835 0 188", shell=True)
+                                time.sleep(5)
         return
 @app.callback(
         Output(component_id='placeholder3', component_property='children'),
         [Input(component_id='rf_on_button3', component_property='n_clicks')])
 def rf3_on(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5265155, tx_pulselength=184)
-                rfdevice.cleanup()            
-#               subprocess.check_output("/var/www/rfoutlet/codesend 5265155 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5265155, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()          
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5265155 -l 184 -p 0", shell=True)  
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5265155 0 188", shell=True)
+                                time.sleep(5)
         return
 @app.callback(
         Output(component_id='placeholder4', component_property='children'),
         [Input(component_id='rf_on_button4', component_property='n_clicks')])
 def rf4_on(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5266691, tx_pulselength=184)
-                rfdevice.cleanup()            
-                #subprocess.check_output("/var/www/rfoutlet/codesend 5266691 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5266691, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()      
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5266691 -l 184 -p 0", shell=True) 
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5266691 0 188", shell=True)
+                                time.sleep(5)
         return
 @app.callback(
         Output(component_id='placeholder5', component_property='children'),
         [Input(component_id='rf_on_button5', component_property='n_clicks')])
 def rf5_on(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5272835, tx_pulselength=184)
-                rfdevice.cleanup()
-                #subprocess.check_output("/var/www/rfoutlet/codesend 5272835 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5272835, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5272835 -l 188 -p 0", shell=True)  
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5272835 0 188", shell=True)
+                                time.sleep(5)
         return
 #OFF
 @app.callback(
@@ -872,52 +912,90 @@ def rf5_on(n_clicks):
         [Input(component_id='rf_off_button1', component_property='n_clicks')])
 def rf1_off(n_clicks):
         if (n_clicks):
-                subprocess.check_output("/var/www/rfoutlet/codesend 5264700 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5264700, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5264700 -l 188 -p 0", shell=True)  
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5264700 0 188", shell=True)
+                                time.sleep(5)
         return
 @app.callback(
         Output(component_id='placeholder7', component_property='children'),
         [Input(component_id='rf_off_button2', component_property='n_clicks')])
 def rf2_off(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5264844, tx_pulselength=184)
-                rfdevice.cleanup()
-                #subprocess.check_output("/var/www/rfoutlet/codesend 5264844 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5264844, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5264844 -l 184 -p 0", shell=True)
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5264844 0 188", shell=True)
+                                time.sleep(5)
         return
 @app.callback(
         Output(component_id='placeholder8', component_property='children'),
         [Input(component_id='rf_off_button3', component_property='n_clicks')])
 def rf3_off(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5265164, tx_pulselength=184)
-                rfdevice.cleanup()
-                #subprocess.check_output("/var/www/rfoutlet/codesend 5265164 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5265164, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5265164 -l 184 -p 0", shell=True)
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5265164 0 188", shell=True) 
+                                time.sleep(5)
         return
 @app.callback(
         Output(component_id='placeholder9', component_property='children'),
         [Input(component_id='rf_off_button4', component_property='n_clicks')])
 def rf4_off(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5266700, tx_pulselength=184)
-                rfdevice.cleanup()
-                #subprocess.check_output("/var/www/rfoutlet/codesend 5266700 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5266700, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5266700 -l 184 -p 0", shell=True)
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5266700 0 188", shell=True) 
+                                time.sleep(5)
         return
 @app.callback(
         Output(component_id='placeholder10', component_property='children'),
         [Input(component_id='rf_off_button5', component_property='n_clicks')])
 def rf5_off(n_clicks):
         if (n_clicks):
-                rfdevice = RFDevice(17)
-                GPIO.setmode(GPIO.BCM)
-                rfdevice.enable_tx()
-                rfdevice.tx_code(code=5272844, tx_proto=1, tx_pulselength=184, tx_length=24)
-                rfdevice.cleanup()
-                #subprocess.check_output("/var/www/rfoutlet/codesend 5272844 -l 184 -p 0", shell=True)  
+                if(rpi_rf_433):
+                        rfdevice = RFDevice(17)
+                        GPIO.setmode(GPIO.BCM)
+                        rfdevice.enable_tx()
+                        rfdevice.tx_code(code=5272844, tx_proto=0, tx_pulselength=188, tx_length=24)
+                        rfdevice.cleanup()
+                else:
+                        for ping in range(0,5):
+                                #Fire 10 times for reliability
+                                #subprocess.check_output("/var/www/rfoutlet/codesend 5272844 -l 184 -p 0", shell=True)
+                                subprocess.check_output("/var/www/433Utils/RPi_utils/codesend 5272844 0 188", shell=True)  
+                                time.sleep(5)
         return
 
 #Update time
